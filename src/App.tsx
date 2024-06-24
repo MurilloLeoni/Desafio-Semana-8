e5fc2e373b20a376928ed2bc386b3bf20703bfb7
weu-souza <weuller1561@gmail.com>
2024-06-24 19:15:37 -0300

    configuração inicial do router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-// import './App.css'
+import { BrowserRouter,Route, Routes } from "react-router-dom"
+import Footer from "./shared/footer/Footer"
+import Header from "./shared/header/Header"
 
 
 function App() {
  
  
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={"pagina coloca aqui"}></Route>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
   )
 }
 
