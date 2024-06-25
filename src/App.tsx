fc4fc016cac436bb32f4d745825a6f6ff554abf8
Murillo Leoni <murilloleoni1999@gmail.com>
2024-06-25 18:41:14 -0300

    Finalização tela_loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Route, Routes } from "react-router-dom"
 import Footer from "./shared/footer/Footer"
 import Header from "./shared/header/Header"
 import Login from "./pages/Login"
+import Loader from "./components/Loader"
 
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
       <Route path="/" element={<Login/>}></Route>
     </Routes>
     {/* <Footer/> */}
+    <Routes>
+      <Route path="/loading" element={<Loader/>}></Route>
+    </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import styles from "/src/components/Loader.module.css"
+
+const Loader = () => {
+  return (<>
+  <div className={styles.circle}></div>
+  </>
+  )
+}
+
+export default Loader
