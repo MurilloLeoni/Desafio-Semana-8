c086e73ddeed94a153d1c88313f0a67e6cceaa42
Murillo  Sousa <murillosm@hotmail.com>
2024-06-26 14:50:08 -0300

    Header mobile

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -18,27 +18,27 @@ const Header = () => {
 		console.log("Mudou a rota");
 	}, [location]);
 
-	// esta função é chamada para mudar a cor do texto quando a rota é ativa
+	// esta função é chamada para mudar a cor do texto da navegação quando a rota é ativa
 	const linkStyle = ({ isActive }: { isActive: boolean }) =>
-		`p-4 text-white inline-flex gap-2 body-review ${isActive ? "text-[#02E7F5]" : ""
+		`p-4 text-white inline-flex gap-2 body-review ${isActive ? "text-cyan-400" : ""
 		}`;
-	
-	const searchStyle = `p-4 text-white inline-flex gap-2 body-review`;
+
+	const searchStyle = `sm:p-4 p-2  text-white inline-flex gap-2 body-review`;
 
 	// esta função é chamada para mudar a cor do ícone quando a rota é ativa
 	const iconStyle = ({ isActive }: { isActive: boolean }) =>
-		`fill-white ${isActive ? "fill-[#02E7F5]" : ""}`;
+		`fill-white ${isActive ? "fill-cyan-400" : ""}`;
 
 	return (
-		<div className="w-full h-[100.46px] px-8 py-4 bg-gradient-to-b from-neutral-950 to-neutral-0 justify-between items-center inline-flex">
-			<div className="h-full justify-between items-center gap-4 flex w-full ">
-				<div className="inline-flex  items-center">
-					<div className="relative">
-						<NavLink to="/" className={""} end>
-							<img src={logo} alt="Logo" className="w-[200px] h-[68.46px]" />
-						</NavLink>
-					</div>
-					<div className="justify-start items-start flex ">
+		<header className="w-full xl:h-[100.46px]  sm:px-8 py-4 bg-gradient-to-b from-neutral-950 to-neutral-0 justify-between items-center inline-flex">
+			<div className="flex xl:flex-row flex-col items-center w-full ">
+				<div className="relative">
+					<NavLink to="/" className={""} end>
+						<img src={logo} alt="Logo" className="w-[200px] h-[68.46px]" />
+					</NavLink>
+				</div>
+				<div className="w-full flex items-center flex-col-reverse lg:flex-row">
+					<div className=" justify-center items-start md:items-center flex text-center sm:block">
 						<nav className="p-4">
 							<NavLink to="/" className={linkStyle} end>
 								{({ isActive }) => (
@@ -74,22 +74,24 @@ const Header = () => {
 							</NavLink>
 						</nav>
 					</div>
-				</div>
-				<div className="flex h-12 gap-4 items-center ">
-					<button className={searchStyle}>
-						<IconSearch className="w-6 h-6" />
-						<p>Buscar</p>
-					</button>
-					<button className={searchStyle}>
-						<IconPlus className="w-6 h-6" />
-						<p>Minha lista</p>
-					</button>
-					<button className="w-12 h-12 rounded-full bg-secondary">
-						<img className="w-12 h-12 rounded-full border-2 border-cyan-400" src="https://via.placeholder.com/48x48" />
-					</button>
+					<div className="flex h-12 items-center w-auto lg:ml-auto lg:justify-center">
+						<nav className="min-[320px]:flex-row gap-1 flex ">
+							<button className={searchStyle}>
+								<IconSearch className="w-6 h-6" />
+								<p>Buscar</p>
+							</button>
+							<NavLink to="not" className={searchStyle}>
+								<IconPlus className="w-6 h-6" />
+								<p>Minha lista</p>
+							</NavLink>
+						</nav>
+						<button className="w-12 h-12 rounded-full min-[400px]:mx-4 mx-1">
+							<img className="w-12 h-12 rounded-full border-2 border-cyan-400" src="https://via.placeholder.com/48x48" alt="user" />
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
+		</header>
 	);
 };
 
